85e448c8c04b2d184e1860c8aad90fc0804b8069
dapeeche <vladkuts2001@gmail.com>
2023-11-30 21:22:07 +0200

    DZ_8 complete

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+interface DataBase {
+    public function getData();
+}
+class Mysql implements DataBase
+{
+    public function getData()
+    {
+        return 'some data from database';
+    }
+}
+
+class Controller
+{
+    private $adapter;
+
+    public function __construct(DataBase $mysql)
+    {
+        $this->adapter = $mysql;
+    }
+
+    function getData()
+    {
+        $this->adapter->getData();
+    }
+}
